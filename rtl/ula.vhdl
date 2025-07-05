d8da7425da658f6a66a33c2fcddba3131ddf598d
jpdarabas <jpdarabas@gmail.com>
2025-07-04 21:55:06 -0300

    ula completa

--- rtl/ula.vhdl
+++ rtl/ula.vhdl
@@ -14,10 +14,33 @@ end entity;
 ARCHITECTURE behaviour OF ula IS
 signal add, sub, mul, mac : std_logic_vector(15 downto 0); -- Sinais intermediários para operações
 BEGIN
-    add <= A + B; -- ALTERAR PARA BFLOAT16
-    sub <= A - B; -- ALTERAR PARA BFLOAT16
-    mul <= A * B; -- ALTERAR PARA BFLOAT16
-    mac <= C + mul; -- ALTERAR PARA BFLOAT16
+    addsomasub: entity work.somasub
+    port map (
+        A => A,
+        B => B,
+        op => '0',
+        S => add
+    );
+    subsomasub: entity work.somasub
+    port map (
+        A => A,
+        B => B,
+        op => '1',
+        S => sub
+    );
+    mulmul: entity work.mul
+    port map (
+        A => A,
+        B => B,
+        S => mul
+    );
+    macsomasub: entity work.somasub
+    port map (
+        A => C,
+        B => mul, -- Multiplicação de A e B
+        op => '0', -- Soma para MAC
+        S => mac
+    );
 
     PROCESS(A, B, C, OP)
     BEGIN
@@ -27,7 +50,9 @@ BEGIN
             WHEN "001" => -- Subtração (A - B)
                 S <= sub;
             WHEN "010" => -- AND bit a bit
-        
+                S <= A and B; -- Operação AND bit a bit
+            WHEN "011" => -- OR bit a bit
+                S <= A or B; -- Operação OR bit a bit
             WHEN  "011" => -- OR bit a bit
 
             WHEN "100" => -- A > B
@@ -35,14 +60,14 @@ BEGIN
                 IF sub(0) = '1' THEN
                     S <= "0000000000000000"; -- S <= 0 bfloat16
                 ELSE
-                    -- S <= 1 bfloat16
+                    S <= "0011111110000000"; -- S <= 1 bfloat16
                 END IF;
             WHEN "101" => -- A * B
                 S <= mul;
             WHEN "110" => -- ReLU
             -- ReLU(A) = max(0, A) no formato bfloat16
-                if A(15) == '1' THEN
-                -- S <= 0 BFLOAT
+                if A(15) = '1' THEN
+                    S <= "0000000000000000"; -- S <= 0 bfloat16
                 ELSE
                     S <= A; -- Retorna A se for positivo
                 END IF;
